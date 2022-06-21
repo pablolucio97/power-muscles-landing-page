f08b7b86ee4e2ba840874af604ac40b722757525
Pablo <pablolucio_@hotmail.com>
2022-06-21 07:47:29 -0300

    Improving layout#2

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,6 @@
 export const testimonials = [
   {
-    url: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    url: "/girl1.jpg",
     name: "Lorena Santos",
     role: "Designer",
     ratings: 5,
@@ -8,7 +8,7 @@ export const testimonials = [
       "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
   },
   {
-    url: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80",
+    url: "/girl3.jpg",
     name: "Marcela de Oliveira",
     role: "Full-stack developer",
     ratings: 4,
@@ -16,7 +16,7 @@ export const testimonials = [
       "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
   },
   {
-    url: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    url: "/boy1.jpg",
     name: "Márcio José",
     role: "Front-end developer",
     ratings: 5,
@@ -24,7 +24,7 @@ export const testimonials = [
       "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
   },
   {
-    url: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    url: "/girl2.jpg",
     name: "Lorena Campos",
     role: "Designer",
     ratings: 5,
@@ -32,7 +32,7 @@ export const testimonials = [
       "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
   },
   {
-    url: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80",
+    url: "/girl4.jpg",
     name: "Pâmela Silva",
     role: "Full-stack developer",
     ratings: 4,
@@ -40,7 +40,7 @@ export const testimonials = [
       "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
   },
   {
-    url: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    url: "/boy2.jpg",
     name: "Marcos Xavier",
     role: "Front-end developer",
     ratings: 5,
@@ -48,15 +48,15 @@ export const testimonials = [
       "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
   },
   {
-    url: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-    name: "Lorena Silva",
+    url: "/boy3.jpg",
+    name: "Carlos Santana",
     role: "Designer",
     ratings: 5,
     testimonial:
       "Os treinos são sensacionais, estou treinando em casa desde o inicio da quarentena, e estou adorando!",
   },
   {
-    url: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80",
+    url: "/boy4.jpg",
     name: "Paulo Henrique",
     role: "Full-stack developer",
     ratings: 4,
@@ -64,7 +64,7 @@ export const testimonials = [
       "Seus treinos fizeram muita diferença no meu corpo e na minha saúde mental nessa Pandemia!",
   },
   {
-    url: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    url: "/boy5.jpg",
     name: "Bruno Simões",
     role: "Front-end developer",
     ratings: 5,
